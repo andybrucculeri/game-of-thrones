f9f6080df05ea41e24bfdde095e148f61501ca83
Andrea Brucculeri <andrea@brucculeri.com>
2019-04-29 15:11:40 -0400

    add initial site files

---
+++ precache-manifest.678a832981b8a67b8b99bff812fa54ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "200f5e3cf9144472187fdc19585eb984",
+    "url": "/game-of-thrones/static/media/got.200f5e3c.png"
+  },
+  {
+    "revision": "f5fbe5b23a6b9ecd64b27bc0c265a1e8",
+    "url": "/game-of-thrones/static/media/characters.f5fbe5b2.png"
+  },
+  {
+    "revision": "6c9c0fc2a6f53506dac0086a71d99144",
+    "url": "/game-of-thrones/static/media/background.6c9c0fc2.png"
+  },
+  {
+    "revision": "a934c0e9cb2ae2debc38",
+    "url": "/game-of-thrones/static/js/runtime~main.a581667f.js"
+  },
+  {
+    "revision": "4d33d1f735721ac2e531",
+    "url": "/game-of-thrones/static/js/main.d33a6fe9.chunk.js"
+  },
+  {
+    "revision": "4fbd73b7665f10122e55",
+    "url": "/game-of-thrones/static/js/2.104baa16.chunk.js"
+  },
+  {
+    "revision": "4d33d1f735721ac2e531",
+    "url": "/game-of-thrones/static/css/main.7ab2aa78.chunk.css"
+  },
+  {
+    "revision": "ff3288de100fee3766bf746bf1933bed",
+    "url": "/game-of-thrones/index.html"
+  }
+];
